f275c9179bf820cb463a097fd440dc8bd7bb51a6
Kevin <kevinleitesolar@hotmail.com>
2026-04-02 10:43:30 -0300

    chore: fix some imports and add ISR to the page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,5 @@
 import { headers as getHeaders } from "next/headers.js"
 import { getPayload } from "payload"
-import { fileURLToPath } from "url"
 
 import config from "@/payload.config"
 import "./style/globals.css"
@@ -11,13 +10,11 @@ import { Header } from "./components/header"
 import { ExternalLinks } from "./components/external-links"
 import { SocialLinks } from "./components/social-links"
 
+export const revalidate = 3600
+
 export default async function HomePage() {
-  const headers = await getHeaders()
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
-  // const { user } = await payload.auth({ headers })
-
-  // const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
     <main className="container mx-auto min-h-screen flex items-center justify-center">
